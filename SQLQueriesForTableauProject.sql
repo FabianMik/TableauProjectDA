28a78739364459d771a331ce9b6ddfd882fa65a3
FabianMik <81294695+FabianMik@users.noreply.github.com>
2021-08-23 11:34:25 +0200

    Add files via upload

--- SQLQueriesForTableauProject.sql
+++ SQLQueriesForTableauProject.sql
@@ -27,41 +27,44 @@ order by 1,2
 -- looking at total cases vs total deaths
 -- P(dying | covid)
 
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PorftolioProject..CovidDeaths
---Where location like '%states%'
-where continent is not null 
---Group By date
+select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+from PorftolioProject..CovidDeaths
+--where location like '%states%'
+where continent != '' 
+--group By date
 order by 1,2
 
 
 --2 Visualization
 -- Eliminating redundancies
 
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PorftolioProject..CovidDeaths
---Where location like '%states%'
-Where continent is null 
+select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+from PorftolioProject..CovidDeaths
+--where location like '%states%'
+where continent = ''
 and location not in ('World', 'European Union', 'International')
-Group by location
+group by location
 order by TotalDeathCount desc
 
 
 --3 Visualization
 -- countries with highest infection rate compared to population
 
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PorftolioProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population
+select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+from PorftolioProject..CovidDeaths
+--where location like '%states%'
+group by Location, Population
 order by PercentPopulationInfected desc
 
+select total_cases
+from PorftolioProject..CovidDeaths
+Where location like '%states%'
 
 --4 Visualization
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PorftolioProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population, date
+select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+from PorftolioProject..CovidDeaths
+--where location like '%states%'
+group by Location, Population, date
 order by PercentPopulationInfected desc
 
 
